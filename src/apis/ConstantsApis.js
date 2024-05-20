8717e8320b0e74972f0ed5cc92adca4d72a4a748
DESKTOP-C2EF9Q9\Genergy <tnrlaos@gmail.com>
2024-05-20 16:11:39 +0900

    Service key 상수 추가

--- src/apis/ConstantsApis.js
+++ src/apis/ConstantsApis.js
@@ -1,2 +1,3 @@
 export const GET_VILAGE_FCST = 'http://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getVilageFcst';
+export const SERVICE_KEY = 'pssWI8FkQmDOOOz8ydNxIKn1QaRT%2FauH15VvCgA5x0LMRdlPPH%2BpfBuYs1Nyr%2FNUjkt0%2BU1mTOlLT1%2BoISxxsQ%3D%3D'
 
