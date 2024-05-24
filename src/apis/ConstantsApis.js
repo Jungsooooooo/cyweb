7a1eae884336fba3562a61568d8a6fd30b07f231
DESKTOP-C2EF9Q9\Genergy <tnrlaos@gmail.com>
2024-05-24 13:55:04 +0900

    버전 상수 api 추가

--- src/apis/ConstantsApis.js
+++ src/apis/ConstantsApis.js
@@ -1,3 +1,4 @@
 export const GET_VILAGE_FCST = 'http://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getVilageFcst';
+export const GET_VILAGE_FCST_SHORT = 'http://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getFcstVersion'
 export const SERVICE_KEY = 'pssWI8FkQmDOOOz8ydNxIKn1QaRT%2FauH15VvCgA5x0LMRdlPPH%2BpfBuYs1Nyr%2FNUjkt0%2BU1mTOlLT1%2BoISxxsQ%3D%3D'
 
