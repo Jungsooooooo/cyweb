8daf058447719fcc9dfb1b7cd4273d037e465ed2
jungsoooooo <tnrlaos@gmail.com>
2024-06-05 13:59:34 +0900

    미세먼지 서비스키 추가

--- src/apis/ConstantsApis.js
+++ src/apis/ConstantsApis.js
@@ -3,3 +3,6 @@ export const GET_VILAGE_FCST_SHORT = "http://apis.data.go.kr/1360000/VilageFcstI
 export const SERVICE_KEY =
   "pssWI8FkQmDOOOz8ydNxIKn1QaRT%2FauH15VvCgA5x0LMRdlPPH%2BpfBuYs1Nyr%2FNUjkt0%2BU1mTOlLT1%2BoISxxsQ%3D%3D";
 export const weatherpicsrc = '<a href="http://www.freepik.com">Designed by Titusurya / Freepik</a>';
+
+export const SERVICE_KEY_FINEDUST =
+  "pssWI8FkQmDOOOz8ydNxIKn1QaRT%2FauH15VvCgA5x0LMRdlPPH%2BpfBuYs1Nyr%2FNUjkt0%2BU1mTOlLT1%2BoISxxsQ%3D%3D";
