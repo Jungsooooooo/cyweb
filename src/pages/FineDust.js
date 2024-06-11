71f3d7bd06ef0483484ff50261a0693ca2996f80
jungsoooooo <tnrlaos@gmail.com>
2024-06-11 16:51:59 +0900

    미세먼지 표시 진행중

--- src/pages/FineDust.js
+++ src/pages/FineDust.js
@@ -1,41 +1,68 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
+import { ResponsiveBullet } from "@nivo/bullet";
+import "../css/FineDust.css";
 import { SERVICE_KEY_FINEDUST } from "../apis/ConstantsApis";
 
-import "../css/FineDust.css";
-
 const FineDust = () => {
-  let today = new Date();
-  let nowDate = today.getDate() < 10 ? "0" + today.getDate() : today.getDate();
-  let todayDate = today.getFullYear() + "-" + "0" + (today.getMonth() + 1) + "-" + nowDate;
+  const today = new Date();
+  const nowDate = today.getDate() < 10 ? "0" + today.getDate() : today.getDate();
+  const todayDate = `${today.getFullYear()}-0${today.getMonth() + 1}-${nowDate}`;
   console.log(todayDate);
+  const [value, setValue] = useState(0);
+
   useEffect(() => {
     getFineData();
+    getFineData2();
   }, []);
 
   const getFineData = () => {
     var xhr = new XMLHttpRequest();
-    var url = "http://apis.data.go.kr/B552584/ArpltnInforInqireSvc/getMinuDustFrcstDspth"; /*URL*/
-    var queryParams = "?" + encodeURIComponent("serviceKey") + "=" + SERVICE_KEY_FINEDUST; /*Service Key*/
-    queryParams += "&" + encodeURIComponent("returnType") + "=" + encodeURIComponent("json"); /**/
-    queryParams += "&" + encodeURIComponent("numOfRows") + "=" + encodeURIComponent("100"); /**/
-    queryParams += "&" + encodeURIComponent("pageNo") + "=" + encodeURIComponent("1"); /**/
-    queryParams += "&" + encodeURIComponent("searchDate") + "=" + encodeURIComponent(todayDate); /**/
-    // queryParams += "&" + encodeURIComponent("InformCode") + "=" + encodeURIComponent("PM10"); /**/
+    var url = "http://apis.data.go.kr/B552584/ArpltnInforInqireSvc/getMinuDustFrcstDspth";
+    var queryParams = "?" + encodeURIComponent("serviceKey") + "=" + SERVICE_KEY_FINEDUST;
+    queryParams += "&" + encodeURIComponent("returnType") + "=" + encodeURIComponent("json");
+    queryParams += "&" + encodeURIComponent("numOfRows") + "=" + encodeURIComponent("100");
+    queryParams += "&" + encodeURIComponent("pageNo") + "=" + encodeURIComponent("1");
+    queryParams += "&" + encodeURIComponent("searchDate") + "=" + encodeURIComponent(todayDate);
     xhr.open("GET", url + queryParams);
     xhr.onreadystatechange = function () {
-      if (this.readyState == 4) {
+      if (this.readyState === 4) {
         const tmpData = this.responseText;
-
         const jsonD = JSON.parse(tmpData);
         console.log({ jsonD });
       }
     };
     xhr.send("");
   };
+
+  const getFineData2 = () => {
+    var xhr = new XMLHttpRequest();
+    var url = "http://apis.data.go.kr/B552584/ArpltnInforInqireSvc/getMsrstnAcctoRltmMesureDnsty";
+    var queryParams = "?" + encodeURIComponent("serviceKey") + "=" + SERVICE_KEY_FINEDUST;
+    queryParams += "&" + encodeURIComponent("returnType") + "=" + encodeURIComponent("json");
+    queryParams += "&" + encodeURIComponent("numOfRows") + "=" + encodeURIComponent("100");
+    queryParams += "&" + encodeURIComponent("pageNo") + "=" + encodeURIComponent("1");
+    queryParams += "&" + encodeURIComponent("stationName") + "=" + encodeURIComponent("종로구");
+    queryParams += "&" + encodeURIComponent("dataTerm") + "=" + encodeURIComponent("DAILY");
+    queryParams += "&" + encodeURIComponent("ver") + "=" + encodeURIComponent("1.0");
+    xhr.open("GET", url + queryParams);
+    xhr.onreadystatechange = function () {
+      if (this.readyState === 4) {
+        const tmpData = this.responseText;
+        const AjsonD = JSON.parse(tmpData);
+        console.log({ AjsonD });
+        setValue(30);
+      }
+    };
+    xhr.send("");
+  };
+
   return (
-    <>
-      <div className="finemain">fineDust</div>
-    </>
+    <div className="finemain">
+      <h1>Fill Rectangle</h1>
+      <div className="rectangle">
+        <div className="fill" style={{ width: `${value}%` }}></div>
+      </div>
+    </div>
   );
 };
 
