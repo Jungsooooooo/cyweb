77832d070c4f37f7f25dc464ee8b082bddb7020e
jungsoooooo <tnrlaos@gmail.com>
2024-06-25 14:28:37 +0900

    먼지 얻어오는 API 설정

--- src/apis/ConstantsApis.js
+++ src/apis/ConstantsApis.js
@@ -1,5 +1,6 @@
-export const GET_VILAGE_FCST = "http://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getVilageFcst";
-export const GET_VILAGE_FCST_SHORT = "http://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getFcstVersion";
+export const GET_VILAGE_FCST = "https://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getVilageFcst";
+export const GET_VILAGE_FCST_SHORT = "https://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getFcstVersion";
+export const GET_DUST = "http://apis.data.go.kr/B552584/ArpltnInforInqireSvc/getMinuDustFrcstDspth";
 export const SERVICE_KEY =
   "pssWI8FkQmDOOOz8ydNxIKn1QaRT%2FauH15VvCgA5x0LMRdlPPH%2BpfBuYs1Nyr%2FNUjkt0%2BU1mTOlLT1%2BoISxxsQ%3D%3D";
 export const weatherpicsrc = '<a href="http://www.freepik.com">Designed by Titusurya / Freepik</a>';
