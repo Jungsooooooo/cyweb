efb6becd398fb8c5be3554e5f060bd2d46d1c677
jungsoooooo <tnrlaos@gmail.com>
2024-06-25 14:23:21 +0900

    public url 추가

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import HomeView from "./home/HomeView";
 const App = () => {
   return (
     <>
-      <BrowserRouter>
+      <BrowserRouter basename={process.env.PUBLIC_URL}>
         <Routes>
           <Route path="/" element={<HomeView />} />
         </Routes>
